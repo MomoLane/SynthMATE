38ff728cfee13ecf7977833797291dab29871234
MomoLane <ama.moeng@gmail.com>
2024-12-07 05:43:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     },
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3.0",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.9",
