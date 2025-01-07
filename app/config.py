402e2afcbf056bb723125b6cb7c7d189acda734a
MomoLane <ama.moeng@gmail.com>
2025-01-07 22:41:56 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,16 @@
 CONFIG = {
-    "arduino_port": "COM4",
+    "arduino_port": "COM9",
     "camera_index": 0,
     "image_save_path": "images/",
-    "excel_file_path": "reaction_data.xlsx"
+    "excel_file_path": "reaction_data.xlsx",
+    "pump_pins": {
+        2: 14.974, #these are the volume flow rates in mL/s
+        3: 14.201,
+        4: 13.373,
+        5: 14.262,
+        6: 14.251,
+        7: 14.422,
+        8: 14.914,
+        9: 15.384
+    }
 }
